e732bbc96b5fac97747dc2ed3b3020932c72fcef
V1adimirrr <bondarchuk322@mail.ru>
2022-12-31 17:06:53 +0700

    refactor

--- src/games/even.js
+++ src/games/even.js
@@ -1,55 +1,15 @@
-import readlineSync from 'readline-sync';
+import { getRandomNumber, getEvenNumber, startGame } from '../index.js';
 
-const getRandomNumebr = (max) => Math.floor(Math.random() * max);
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const getEvenNumber = (number) => number % 2 === 0;
+const corectAnswer = () => {
+  const number = getRandomNumber(25);
+  const question = String(number);
+  const answer = getEvenNumber(number) ? 'yes' : 'no';
 
-const corectAnswer = (number, userAnswer) => {
-  const possibleAnswers = ['yes', 'no'];
-
-  let flag;
-  if (getEvenNumber(number) === true && userAnswer === possibleAnswers[0]) {
-    flag = true;
-  } else if (
-    getEvenNumber(number) === false &&
-    userAnswer === possibleAnswers[1]
-  ) {
-    flag = true;
-  }
-
-  return flag;
+  return [question, answer];
 };
 
-const tryAgain = (answer, userName) => {
-  if (answer === 'yes') {
-    return console.log(
-      `'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`
-    );
-  }
-  return console.log(
-    `'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`
-  );
-};
-
-const brainEven = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`${'Hello,'} ${userName}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  for (let i = 0; i < 3; i += 1) {
-    const randomNumber = getRandomNumebr(100);
-    console.log('Question:', randomNumber);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (corectAnswer(randomNumber, userAnswer)) {
-      console.log('Correct!');
-    } else if (!corectAnswer(randomNumber, userAnswer)) {
-      return tryAgain(userAnswer, userName);
-    }
-  }
-
-  return console.log(`${'Congratulations,'} ${userName}!`);
-};
+const brainEven = () => startGame(rules, corectAnswer);
 
 export default brainEven;
