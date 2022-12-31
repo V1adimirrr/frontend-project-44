12f9843e3d9e2884b26aa272cd871a1e9052af19
V1adimirrr <bondarchuk322@mail.ru>
2022-12-31 17:07:43 +0700

    feat

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import readlineSync from 'readline-sync';
+
+const getRandomNumber = (max) => Math.floor(Math.random() * max);
+
+const getEvenNumber = (number) => number % 2 === 0;
+
+const startGame = (rules, corectAnswer) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`${'Hello,'} ${userName}!`);
+  console.log(rules);
+
+  for (let i = 0; i < 3; i += 1) {
+    const [question, answer] = corectAnswer(i);
+    console.log('Question:', question);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer !== answer) {
+      console.log(
+        `${userAnswer} is wrong answer ;(. Correct answer was '${answer}'`
+      );
+      return console.log(`Let's try again, ${userName}`);
+    }
+    console.log('Correct!');
+  }
+
+  return console.log(`Congratulations, ${userName}!`);
+};
+
+export { getRandomNumber, getEvenNumber, startGame };
