64870885c54e19fd76b3f2d4b6131d53f9ceee51
V1adimirrr <bondarchuk322@mail.ru>
2023-01-04 01:48:53 +0700

    feat: create a game is a prime number

---
+++ src/games/prime.js
@@ -0,0 +1,25 @@
+import { getRandomNumber, startGame } from '../index.js';
+
+const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return number > 1;
+};
+
+const corectAnswer = () => {
+  const randomNumber = getRandomNumber(2, 20);
+
+  const question = String(randomNumber);
+  const answer = isPrime(randomNumber) ? 'yes' : 'no';
+
+  return [question, answer];
+};
+
+const brainPrime = () => startGame(rules, corectAnswer);
+
+export default brainPrime;
