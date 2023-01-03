f0789376ad1dac182163da2655a0dbe54766c40c
V1adimirrr <bondarchuk322@mail.ru>
2023-01-03 22:09:12 +0700

    feat: create a game arithmetic progression

---
+++ src/games/progression.js
@@ -0,0 +1,31 @@
+import { getRandomNumber, startGame } from '../index.js';
+
+const rules = 'What number is missing in the progression?';
+
+const getProgression = (start, step, lengthProgression) => {
+  const result = [];
+  for (let i = 0; result.length < lengthProgression; i += 1) {
+    result.push(start + step * i);
+  }
+
+  return result;
+};
+
+const corectAnswer = () => {
+  const start = getRandomNumber(0, 20);
+  const step = getRandomNumber(1, 5);
+  const length = getRandomNumber(5, 10);
+
+  const hiddenIndex = getRandomNumber(0, length);
+  const progression = getProgression(start, step, length);
+
+  const answer = progression[hiddenIndex];
+  progression[hiddenIndex] = '..';
+  const question = progression.join(' ');
+
+  return [question, String(answer)];
+};
+
+const brainPorgression = () => startGame(rules, corectAnswer);
+
+export default brainPorgression;
