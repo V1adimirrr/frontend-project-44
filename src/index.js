f37bd99b819a9b28f47c74f3c25bed9f1c9606dc
V1adimirrr <bondarchuk322@mail.ru>
2023-01-09 02:44:53 +0700

    refactor: add round counter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import readlineSync from 'readline-sync';
 
+const roundsCount = 3;
+
 const runEngine = (rules, makeRound) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`${'Hello,'} ${userName}!`);
   console.log(rules);
 
-  for (let i = 0; i < 3; i += 1) {
+  for (let i = 0; i < roundsCount; i += 1) {
     const [question, answer] = makeRound(i);
     console.log('Question:', question);
     const userAnswer = readlineSync.question('Your answer: ');
